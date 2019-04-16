87cd4e1ad0c28f7c234008d05bcd95cc19e3559c
Damian Marzec <damian.marzec@upaid.pl>
2019-04-16 20:04:52 +0200

    implement some functions

--- board.js
+++ board.js
@@ -1,15 +1,43 @@
 function checkBoardParity(colsAmount, rowsAmount) {
-    return true;
-    // @todo @aniax
+    let allTiles = colsAmount * rowsAmount;
+    return (allTiles % 2) === 0;
 }
 
 function getRandomListOfPairsId(colsAmount, rowsAmount) {
-    return [1,4,3,4,2,3,2,1,5,5,6,6,7,7,8,8];
-    // @todo
+
+    // ile jest wszystkich elementow
+    let countAll = colsAmount * rowsAmount;
+
+    // ile par
+    let countPairs = countAll / 2;
+
+    // stworzyc tablice
+    let randomPairs = [];
+
+    do {
+
+        // wylosuj liczbe z odpowiedniego zakresu
+        let randomNumber = Math.ceil(Math.random() * countPairs);
+
+        // policz ile razy ta cyfra wystepuje już w tablicy wylosowanych elem.
+        let occureCount = 0; // nasz licznik
+        for (let i = 0; i < randomPairs.length; i++) { // wykonaj tyle razy ile aktualnie jest elementow w tablicy
+            if (randomPairs[i] === randomNumber) { // czy aktualnie sprawdzany eleemnt jest taki jak nasza wylosowana liczba
+                occureCount++; // jeśli tak, zwiększ licznik o 1
+            }
+        }
+
+        // sprawdz czy takich wylosowanych jest mniej niż nasz max (2)
+        if (occureCount < 2) {
+            randomPairs.push(randomNumber); // dodaj do tablicy
+        }
+    } while (randomPairs.length < countAll);
+
+    return randomPairs;
 }
 
 function generateBoxHtml(pictureId) {
-    return '<div class="game-tile" data-card-type="'+pictureId+'"></div>';
+    return '<div class="game-tile" data-card-type="' + pictureId + '"></div>';
 }
 
 function unCover(clickedId) {
